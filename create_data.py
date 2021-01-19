d96d51e72ca4a97d4b91847ccbb8c2e1c76026ab
Patrick M. Ryan <patrick.ryan@evanschambers.com>
2021-01-19 17:46:45 -0500

    basic reading and writing

--- create_data.py
+++ create_data.py
@@ -7,21 +7,24 @@ from collections import namedtuple
 # https://docs.python.org/3.8/library/struct.html
 
 data_format = [
-    ( 'sensor'        , '8s' ),   # 8
-    ( 'reading_one'   , 'I' ),    # 4
-    ( 'reading_two'   , 'l' ),    # 4
-    ( 'reading_three' , 'f')      # 4
+    ( 'sensor'        , '8s'),   # 8
+    ( 'reading_one'   , 'I'),    # 4
+    ( 'reading_two'   , 'l'),    # 4
+    ( 'reading_three' , 'f'),    # 4
+    ( 'reading_four'  , 'd')     # 8
 ]
 
 sample1 = {
-    'sensor' : b'xyz001',
-    'reading_one' : 8192,
-    'reading_two':  32768,    # 1000000002
-    'reading_three' : 2021.011905
+    'sensor'        : b'xyz001',
+    'reading_one'   : 8192,
+    'reading_two'   : 32768,
+    'reading_three' : 2021.011905,        # od -j 16 -f sample1.binary
+    'reading_four'  : 6683456248.232      # od -j 20 -e sample1.binary
 }
 
 format_string = '='+''.join( [ tuple[1] for tuple in data_format ] )
-packed_data = pack(format_string, sample1['sensor'], sample1['reading_one'], sample1['reading_two'], sample1['reading_three'])
+packed_data = pack(format_string, sample1['sensor'], sample1['reading_one'],
+                    sample1['reading_two'], sample1['reading_three'], sample1['reading_four'])
 
 f = open('sample1.binary','wb')
 f.write(packed_data)
