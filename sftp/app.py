aea3237cad93a347c41a04ba7fd759980248fb14
Patrick M. Ryan <patrick.ryan@evanschambers.com>
2022-05-26 19:00:13 -0400

    initial working version of cdk script fo sftp server

--- sftp/app.py
+++ sftp/app.py
@@ -5,21 +5,11 @@ import aws_cdk as cdk
 
 from sftp.sftp_stack import SftpStack
 
+env = cdk.Environment(
+    account=os.getenv("CDK_DEFAULT_ACCOUNT"), region=os.getenv("CDK_DEFAULT_REGION")
+)
 
 app = cdk.App()
-SftpStack(
-    app,
-    "SftpStack",
-    # If you don't specify 'env', this stack will be environment-agnostic.
-    # Account/Region-dependent features and context lookups will not work,
-    # but a single synthesized template can be deployed anywhere.
-    # Uncomment the next line to specialize this stack for the AWS Account
-    # and Region that are implied by the current CLI configuration.
-    # env=cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
-    # Uncomment the next line if you know exactly what Account and Region you
-    # want to deploy the stack to. */
-    # env=cdk.Environment(account='123456789012', region='us-east-1'),
-    # For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
-)
+SftpStack(app, "SftpStack", env=env)
 
 app.synth()
--- sftp/sftp/sftp_stack.py
+++ sftp/sftp/sftp_stack.py
@@ -1,7 +1,13 @@
+import sys
+import boto3
+
 from aws_cdk import (
     # Duration,
     Stack,
-    # aws_sqs as sqs,
+    aws_iam as iam,
+    aws_ec2 as ec2,
+    aws_transfer as transfer,
+    aws_elasticloadbalancingv2 as elb,
 )
 from constructs import Construct
 
@@ -10,10 +16,81 @@ class SftpStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        # context params:
+        #  vpc name
+        #  CIDRs for security group  (create PrefixList)
+        #  perm boundary (optional)
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "SftpQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        permissions_boundary_policy_arn = self.node.try_get_context(
+            "PermissionsBoundaryPolicyArn"
+        )
+        if permissions_boundary_policy_arn:
+            policy = iam.ManagedPolicy.from_managed_policy_arn(
+                self, "PermissionsBoundary", permissions_boundary_policy_arn
+            )
+            iam.PermissionsBoundary.of(self).apply(policy)
+
+        key = "VpcName"
+        vpc_name = self.node.try_get_context(key)
+        if not vpc_name:
+            print("missing context variable " + key)
+            sys.exit(1)
+
+        key = "CidrRanges"
+        cidr_ranges = self.node.try_get_context(key)
+        if not cidr_ranges:
+            cidr_ranges = []
+
+        vpc = ec2.Vpc.from_lookup(self, "Vpc", vpc_name=vpc_name)
+        subnets = vpc.private_subnets
+        subnet_ids = [subnet.subnet_id for subnet in subnets]
+
+        security_group = ec2.SecurityGroup(self, "SftpSecurityGroup", vpc=vpc)
+        for cidr in cidr_ranges:
+            security_group.add_ingress_rule(ec2.Peer.ipv4(cidr), ec2.Port.tcp(22))
+
+        # nlb = elb.NetworkLoadBalancer(self, 'NLB', vpc=vpc, vpc_subnets=subnets)
+
+        # vpc_endpoint = ec2.VpcEndpoint(self, 'VpcEndpoint')
+
+        vpc_endpoint = ec2.CfnVPCEndpoint(
+            self,
+            "VpcEndpoint",
+            service_name=f"com.amazonaws.{self.region}.transfer",
+            vpc_id=vpc.vpc_id,
+            subnet_ids=subnet_ids,
+            vpc_endpoint_type="Interface",
+        )
+
+        server = transfer.CfnServer(
+            self,
+            "SftpServer",
+            pre_authentication_login_banner="a very important sftp server",
+            endpoint_details=transfer.CfnServer.EndpointDetailsProperty(
+                vpc_id=vpc.vpc_id,
+                security_group_ids=[security_group.security_group_id],
+                subnet_ids=subnet_ids,
+                # vpc_endpoint_id=vpc_endpoint.ref,
+                # address_allocation_ids=["addressAllocationIds"],
+            ),
+            endpoint_type="VPC",  # nope 'VPC_ENDPOINT',
+            protocols=["SFTP"],
+        )
+
+
+# SftpServer:
+#   Type: AWS::Transfer::Server
+#   Properties:
+#     EndpointType: VPC
+#     EndpointDetails:
+#       AddressAllocationIds:
+#         - !GetAtt SftpPublicIp1.AllocationId
+#         - !GetAtt SftpPublicIp2.AllocationId
+#       VpcId: !Ref VpcId
+#       SubnetIds:
+#         - !Ref SubnetId1
+#         - !Ref SubnetId2
+#       SecurityGroupIds:
+#         - !GetAtt AllowedSecurityGroup.GroupId
+#     Protocols:
+#       - SFTP
